2af30d6bb8588a1c5a2ef3701c32fdf38426a63d
GrayIsh-03 <grayish-03@yandex.ru>
2022-06-16 19:16:12 +0900

    change task.go

--- testmod/main.go
+++ testmod/main.go
@@ -1,29 +1,34 @@
 package main
 
-import "Golang/task"
+import (
+	"Golang/paintneed"
+	"Golang/task"
+	"fmt"
+)
 
 func main() {
-	/*
-		message := quote.Hello()
-		fmt.Println(message)
 
-		var total float64
-		amount, err := paintneed.PaintNeeded(12.2, 3)
-		if err != nil {
-			fmt.Println(err)
-		}
-		total += amount
-		amount, err = paintneed.PaintNeeded(8.3, -2.5)
-		if err != nil {
-			fmt.Println(err)
-		}
-		total += amount
-		amount, err = paintneed.PaintNeeded(12, 8)
-		if err != nil {
-			fmt.Println(err)
-		}
-		total += amount
-		fmt.Printf("Total: need %0.2f a liters paint", total)
-	*/
+	/*message := quote.Hello()
+	fmt.Println(message)*/
+
+	// func определяет сколько нужно краски на каждую стену и сколько в общем
+	var total float64
+	amount, err := paintneed.PaintNeeded(12.2, 3)
+	if err != nil {
+		fmt.Println(err)
+	}
+	total += amount
+	amount, err = paintneed.PaintNeeded(8.3, -2.5)
+	if err != nil {
+		fmt.Println(err)
+	}
+	total += amount
+	amount, err = paintneed.PaintNeeded(12, 8)
+	if err != nil {
+		fmt.Println(err)
+	}
+	total += amount
+	fmt.Printf("Total: need %0.2f a liters paint", total)
+
 	task.GuessNum()
 }
