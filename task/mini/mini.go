96c842c5a77be01ee187b52274ba8e6702ca2fbc
GrayIsh-03 <grayish-03@yandex.ru>
2022-06-19 16:26:13 +0900

    added readfile.go and fun Average

--- task/mini/mini.go
+++ task/mini/mini.go
@@ -161,3 +161,14 @@ func EnumerationCharacter() {
 		}
 	}
 }
+
+func Average() {
+	numbers := [3]float64{71.8, 56.2, 89.5}
+	var sum float64
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+	average := sum / sampleCount
+	fmt.Printf("Average: %0.2f\n", average)
+}
