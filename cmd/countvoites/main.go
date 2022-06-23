a49b6c695241af2f5eb0e3b8041e6ff1ec156df1
GrayIsh-03 <grayish-03@yandex.ru>
2022-06-23 22:40:12 +0900

    modified countvoites/main.go

--- cmd/countvoites/main.go
+++ cmd/countvoites/main.go
@@ -11,5 +11,31 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(lines)
+	var names []string // сегмент для хранения имён кандидатов
+	var counts []int   // сегмент с количеством вхождения каждого имени
+
+	for _, line := range lines {
+		matched := false
+
+		for i, name := range names {
+
+			if name == line { // если строка совпадает с текущим именем
+				counts[i]++    // увеличиваем соответствующий счётчик
+				matched = true // устанавливает признак обнаруженного совпадения
+			}
+
+		}
+
+		if matched == !true { // если совпадение не найдено
+			names = append(names, line) // добавить его как новое имя в сегмент
+			counts = append(counts, 1)  // и добавить новый счётчик (текущая строка станет 1-ым вхождением)
+		}
+
+	}
+
+	for i, name := range names {
+		// Выыести каждый элемент из сегмента names и соответствующий элемент из сегмента counts
+		fmt.Printf("%s: %d\n", name, counts[i])
+	}
+
 }
