ea0ea6c2d87dc25863ecb3dc14007c1a9ebdc9ed
GrayIsh-03 <grayish-03@yandex.ru>
2022-06-30 14:54:18 +0900

    modified main.go, functions with defined type are used

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"golang/pkg/calculation"
 )
+
+type subscraiber struct {
+	name   string
+	rate   float64
+	active bool
+}
 
 func main() {
 
@@ -11,4 +18,24 @@ func main() {
 
 	calculation.RocketSpeed()
 
+	subscraiber1 := defaultSubscriber("Aman Singh")
+	subscraiber1.rate = 4.99
+	printInfo(subscraiber1)
+
+	subscraiber2 := defaultSubscriber("Beth Rayn")
+	printInfo(subscraiber2)
+}
+
+func printInfo(s subscraiber) {
+	fmt.Println("Name:", s.name)
+	fmt.Println("Monthly rate:", s.rate)
+	fmt.Println("Active?", s.active)
+}
+
+func defaultSubscriber(name string) subscraiber {
+	var s subscraiber
+	s.name = name
+	s.rate = 5.99
+	s.active = true
+	return s
 }
