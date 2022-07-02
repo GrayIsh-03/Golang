dda7014072e16ebc4dfabb9bedede73311fcd3f7
GrayIsh-03 <grayish-03@yandex.ru>
2022-07-02 20:03:36 +0900

    added methods ToGallons for liters and milliliters

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"golang/pkg/calculation"
-	"golang/pkg/task"
 )
 
+type Liters float64
+type Gallons float64
+type Milliliters float64
+
+func (l Liters) ToGallons() Gallons {
+	return Gallons(l * 0.264)
+}
+
+func (m Milliliters) ToGallons() Gallons {
+	return Gallons(m * 0.000264)
+}
 func main() {
 
 	/*message := quote.Hello()
 	fmt.Println(message)*/
 
 	calculation.RocketSpeed()
-	task.Subscriber()
+
+	soda := Liters(2)
+	fmt.Printf("%0.3f liters equals %0.3f gallons\n", soda, soda.ToGallons())
+	water := Milliliters(500)
+	fmt.Printf("%0.3f milliliters equals %0.3f gallons\n", water, water.ToGallons())
 
 }
