a973aaa1eaff594b2803d10ed6c272eab4ac8f03
GrayIsh-03 <grayish-03@yandex.ru>
2022-07-11 16:03:09 +0900

    modified file pointers/main.go

--- cmd/pointers/main.go
+++ cmd/pointers/main.go
@@ -45,7 +45,7 @@ func main() {
 	//Go посчитает, что поле myField должно содержать указатель. В действительности
 	// это не так, поэтому происходит ошибка.
 	//? fmt.Println(*pointer.old)
-	//заключаем *pointer в круглые скобки, получаем значение myStruct, после
+	//заключаем *pointer в круглые скобки, получаем значение mySubscriber, после
 	//чего можем обратиться к полю структуры.
 	fmt.Println((*pointer).old)
 	//оператор «точка» позволяет обращаться к полям по указателям на структуры
@@ -55,6 +55,14 @@ func main() {
 
 	applyDiscount(&value)
 	fmt.Println(value.rate)
+
+	value = *defaultSubcriber("Jon Ridik")
+	//! Why value.old = 0
+	fmt.Printf("%#v\n", value)
+	valEasy := defaultSubcriber("Hren Gor'kiy")
+	fmt.Printf("%#v\n", &valEasy)
+	printInfo(valEasy)
+
 }
 
 // Объявляем, что функция возвращает указатель на float64.
@@ -85,6 +93,25 @@ func Double(number *int) {
 *int читается «указатель на int».
 */
 
+//todo=====================================================================
+
+// Изменяет поле rate существующей структуры за областью действия функции
 func applyDiscount(s *mySubscriber) {
 	s.rate = 4.99 // присваивание полю структуры по указателю без оператора *
+}
+
+// возвращает адрес оригинальных значений структуры для указателя, не забивая память.
+func defaultSubcriber(name string) *mySubscriber {
+	var s mySubscriber
+	s.name = name
+	s.rate = 5.99
+	s.active = true
+	return &s
+}
+
+// функции передаётся указатель, вызов полей происходит напрямую, оператор * ненужен.
+func printInfo(s *mySubscriber) {
+	fmt.Println("Name:", &s.name)
+	fmt.Println("Monthly rate:", &s.rate)
+	fmt.Println("Active?", &s.active)
 }
