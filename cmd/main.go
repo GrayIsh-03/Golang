45a8d9dc4a37acad8fff01fd531cdb85ba4d801a
GrayIsh-03 <grayish-03@yandex.ru>
2022-07-19 22:50:25 +0900

    using delayed call defer

--- cmd/main.go
+++ cmd/main.go
@@ -1,54 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"golang/internal/deftype"
+	"log"
+	"os"
+	"strconv"
 )
 
-// interface include methods Play and Stop of package deftype file gadget
-type Player interface {
-	Play(string)
-	Stop()
+// Открывает файл и возвращает указатель на него и значение обнаруженной ошибки
+func OpenFile(filename string) (*os.File, error) {
+	fmt.Println("Opening", filename)
+	return os.Open(filename)
 }
 
-func playList(device Player, songs []string) {
-	for _, song := range songs {
-		device.Play(song)
-	}
-	device.Stop()
+// закрывает файл
+func CloseFile(file *os.File) {
+	fmt.Println("Closing file")
+	file.Close()
 }
 
-func TryOut(player Player) {
-	player.Play("Test track")
-	player.Stop()
-	// задаём исходный тип TapePlayer, тогда отработает ветвь else
-	// если удалить = значение типа для player, тогда отработает ветвь ок,
-	// т.к. исходный тип будет TapeRecord for recorder,и ок = true
-	//player = deftype.TapePlayer{}
-	// используется утверждение типа, для преобразования к конкретному типу
-	recorder, ok := player.(deftype.TapeRecorder) // содержит значение TypeRecord
-	// ок=true if recorder содержит значение TapeRecorder, для которого можно вызвать Record
-	// ок предотвращает панику
-	if ok {
-		recorder.Record()
-		// если убрать ветвь else, то при передачи в параметр функции значения
-		// TapePlayer, ситуация паники когда ок = false, как бы игнорируется
-		// методы Play и Stop вызываются, Record нет.
-	} else {
-		fmt.Println("Player was not a TapeRecorder")
+func DataFileFlo(filename string) ([]float64, error) {
+	var numbers []float64
+	file, err := OpenFile(filename)
+	if err != nil {
+		return nil, err
 	}
+	defer CloseFile(file) // используем отложенный вызов, func будет выполнена даже при наличии ошибок в коде
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return nil, err
+		}
+		numbers = append(numbers, number)
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+	return numbers, nil
 }
 
 func main() {
-	// Обновляем переменную для хранения любого значения, под	держивающего Player.
-	var player Player = deftype.TapePlayer{}
-	mixtape := []string{"Jessie's Girls", "Whip it", "9 to 5"}
-	playList(player, mixtape)
-	player = deftype.TapeRecorder{}
-	playList(player, mixtape)
-
-	TryOut(deftype.TapeRecorder{}) // вызовет метод
-	TryOut(deftype.TapePlayer{})   // выводит сообщение о несоответствии
-	// конкретного типа исходному, с помощью 2-го возвр-го значения ок предотвращается паника
-
+	numbers, err := DataFileFlo(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64
+	for _, number := range numbers {
+		sum += number
+	}
+	fmt.Printf("Sum: %0.2f\n", sum)
 }
