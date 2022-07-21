8f1283749a0e0a8e5ada2d2a4a795255ec131a4c
GrayIsh-03 <grayish-03@yandex.ru>
2022-07-21 23:09:35 +0900

    using recursion to read the contents of a directory

--- cmd/directory/filedir.go
+++ cmd/directory/filedir.go
@@ -4,19 +4,47 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 )
 
 func main() {
-	// Получает сегмент с элементами, представляющими содержимое «my_directory».
-	files, err := ioutil.ReadDir("my_directory")
+	// запускаем процесс вызова scanDirectory для каталолга верхнего уровня
+	err := ScanDirectory("C:/Users/hp/Documents/GitHub/Golang")
 	if err != nil {
 		log.Fatal(err)
 	}
+	count(1, 3)
+}
+
+func count(start int, end int) {
+	fmt.Println(start)
+	if start < end {
+		count(start+1, end)
+	}
+	fmt.Printf("Returning from count (%d, %d) call\n", start, end)
+}
+
+func ScanDirectory(path string) error {
+	fmt.Println(path) // выводит текущую директорию
+	// Получает сегмент с элементами, представляющими содержимое «path».
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
 	for _, file := range files {
-		if file.IsDir() {
-			fmt.Println("Directory:", file.Name())
+		// Соединяет путь каталога и имя файла через символ \.
+		filePath := filepath.Join(path, file.Name())
+		if file.IsDir() { // проверка на подкаталог
+			// рекурсивно вызывает ScanDirectory, но на этот раз с путём подкаталога
+			err := ScanDirectory(filePath)
+			if err != nil {
+				return err
+			}
 		} else {
-			fmt.Println("File:", file.Name())
+			// если это файл, то вывести его имя с путём
+			fmt.Println(filePath)
 		}
 	}
+	return nil
+
 }
